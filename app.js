4052b5504b808b8de10ec6ba442bd6864d5f9f6c
Bruno Calace <bruno.calace@gmail.com>
2023-10-10 23:28:17 -0300

    Desafio entregable 4

--- app.js
+++ app.js
@@ -1,17 +1,95 @@
 const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketIO = require('socket.io');
+const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const productRouter = require('./src/routes/products');
 const cartRouter = require('./src/routes/carts');
+const ProductManager = require('./src/productManager');
 
 const app = express();
-const port = 8080;
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const hbs = exphbs.create({ 
+  layoutsDir: path.join(__dirname, 'src', 'public', 'views'),
+  defaultLayout: 'home' 
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('views', path.join(__dirname, 'src', 'public', 'views'));
+app.set('view engine', 'handlebars');
 
 app.use(bodyParser.json());
-
 app.use(express.static('public'));
+
 app.use('/api/products', productRouter);
 app.use('/api/carts', cartRouter);
 
-app.listen(port, () => {
-  console.log(`Servidor Express escuchando en el puerto ${port}`);
+const productManager = new ProductManager();
+
+app.get('/', async (req, res) => {
+  try {
+    const limit = req.query.limit ? parseInt(req.query.limit) : undefined;
+    const products = await productManager.getProducts(limit);
+    res.render('home', { products });
+  } catch (error) {
+    console.error("Error en la ruta principal:", error);
+    res.status(500).json({ error: 'Error interno del servidor' });
+  }
+});
+
+app.get('/realTimeProducts', (req, res) => {
+  res.render('realTimeProducts');
+});
+
+io.on('connection', (socket) => {
+  console.log('Cliente conectado al servidor de Socket.IO.');
+
+  socket.on('nuevoProducto', async (producto) => {
+    try {
+      const nuevoProducto = await productManager.addProduct(producto);
+  
+      const limit = undefined;
+      const products = await productManager.getProducts(limit);
+
+      io.emit('productosActualizados', { products });
+    } catch (error) {
+      console.error("Error al crear un nuevo producto:", error);
+    }
+  });
+
+  socket.on('eliminarProducto', async (productoId) => {
+    try {
+      await productManager.deleteProduct(productoId);
+  
+      const limit = undefined;
+      const products = await productManager.getProducts(limit);
+  
+      io.emit('productosActualizados', { products });
+    } catch (error) {
+      console.error("Error al eliminar un producto:", error);
+    }
+  });
+
+  socket.on('actualizarProductos', async () => {
+    try {
+      const limit = req.query.limit ? parseInt(req.query.limit) : undefined;
+      const products = await productManager.getProducts(limit);
+
+      socket.emit('productosActualizados', { products });
+    } catch (error) {
+      console.error("Error al actualizar productos en tiempo real:", error);
+    }
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado del servidor de Socket.IO.');
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`Servidor en ejecución en el puerto ${PORT}`);
 });
